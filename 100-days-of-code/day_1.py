5e796cb327b3ce745b10ab4c27f5b984b7bca9e3
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-01 21:30:51 +0100

    tidying up and splitting out games and challenges from notes

--- 100-days-of-code/day_1.py
+++ 100-days-of-code/day_1.py
@@ -27,16 +27,3 @@ lastName = input("What is your last name? ")
 
 namesLength = len(firstName) + len(lastName)
 print("\nYour names together, contain " + str(namesLength) + " characters.\n")
-
-# Day 1 Challenge - band name generator
-# User Greeting
-print("Welcome to the Band Name Generator!\n")
-
-# user input city
-city = input("What city did you grow up in?\n")
-
-# user input pet
-pet = input("What is the name of your pet?\n")
-
-# print the band name
-print("A good name for your band could be " + city + " " + pet + "!\n")
---
+++ 100-days-of-code/day_1_band_name_generator.py
@@ -0,0 +1,12 @@
+# Day 1 Challenge - band name generator
+# User Greeting
+print("Welcome to the Band Name Generator!\n")
+
+# user input city
+city = input("What city did you grow up in?\n")
+
+# user input pet
+pet = input("What is the name of your pet?\n")
+
+# print the band name
+print("A good name for your band could be " + city + " " + pet + "!\n")
