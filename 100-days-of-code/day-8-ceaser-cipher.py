edcf3a2f7c0d59bb6e7ec5f543b652fb0fc357e8
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-01 02:27:15 +0100

    day-8-ceaser-cipher.py updated and ceaser_cipher_logo.py added

--- 100-days-of-code/day-8-ceaser-cipher.py
+++ 100-days-of-code/day-8-ceaser-cipher.py
@@ -1,33 +1,52 @@
 # Ceaser Cipher
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+import os
+from ceaser_cipher_logo import logo
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+os.system('cls||clear')
+print(logo)
 
+def ceaser(start_text, shift_amount, cipher_direction):
+    output_text = ""
+    if direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            output_text += alphabet[new_position]
+        else:
+            output_text += char
+    print(f"The {cipher_direction}d text is: {output_text}")
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The encoded text is {plain_text}")
+use_cipher = True
 
+while use_cipher:
+    user_decision = input("Do you want to use the Ceaser Cipher? yes/no:\n").lower()
 
-if direction == "encode":
-    encrypt(plain_text = text, shift_amount = shift)
-elif direction == "decode":
-    decrypt(cipher_text = text, shift_amount = shift)
-else:
-    print("Invalid input.")
+    if user_decision not in ["no", "yes"]:
+        print("Invalid input, please try again.")
+        continue
+
+    elif user_decision == "no":
+        use_cipher = False
+        print("Thank you for trying the ceaser cipher, goodbye!")
+
+    elif user_decision == "yes":
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        if direction not in ["encode", "decode"]:
+            print("Invalid input, please try again.")
+            continue
+
+        text = input("Type your message:\n").lower()
+
+        try:
+            shift = int(input("Type the shift number:\n"))
+        except:
+            print("Invalid input, please try again.")
+            continue
+        if shift > 26:
+            shift = shift % 26
+
+        ceaser(cipher_direction = direction, start_text = text, shift_amount = shift)
