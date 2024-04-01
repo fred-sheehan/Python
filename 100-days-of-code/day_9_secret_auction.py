c4c1b01739044563e16bb0c22a74968635d4cdcb
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-02 00:38:16 +0100

    add day_9_secret_auction.py and gavel_logo.py

--- 100-days-of-code/day_9_secret_auction.py
+++ 100-days-of-code/day_9_secret_auction.py
@@ -1,1 +1,19 @@
+# Blind bid auction program
+import os
+from gavel_logo import logo
 
+def clear():
+    os.system('cls||clear')
+
+clear()
+print(logo)
+
+input("Welcome to the secret auction program.")
+
+while True:
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if more_bidders == 'no':
+        break
+    clear()
