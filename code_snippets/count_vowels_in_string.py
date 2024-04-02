6074891b28f58e45fc2b0a3817934fb520bf88d2
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-02 21:14:43 +0100

    updates

--- code_snippets/count_vowels_in_string.py
+++ code_snippets/count_vowels_in_string.py
@@ -1,13 +1,14 @@
-# You can run this program and enter a string as input. The program will then count and display the number of vowels in the input string.
+# You can run this program and enter a string as input.
+# The program will then count and display the number of vowels in the input string.
 
 def count_vowels(string):
     vowel_count = 0
     vowels = ['a', 'e', 'i', 'o', 'u']
-    
+
     for char in string:
         if char.lower() in vowels:
             vowel_count += 1
-    
+
     return vowel_count
 
 input_string = input("Enter a string: ")
