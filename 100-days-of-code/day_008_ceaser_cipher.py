efc22db449f13c02fa94a14f3415d6a3ba40f723
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-02 19:45:01 +0100

    added reverse_string.py example and renamed day_*  files to better display in correct order in the terminal

--- 100-days-of-code/day_008_ceaser_cipher.py
+++ 100-days-of-code/day_008_ceaser_cipher.py
@@ -2,7 +2,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 import os
-from ceaser_cipher_logo import logo
+from 8_ceaser_cipher_logo import logo
 
 os.system('cls||clear')
 print(logo)
--- reverse_string.py
+++ reverse_string.py
@@ -1,12 +1,17 @@
 # Function to reverse a string
+
+    # reversing a string using slicing - fastest/best way
 def reverse_string(input_string):
     return input_string[::-1]
 
-# Taking input from the user
-input_string = input("Enter a to reverse: ")
+    # taking string from variable
+input_string = "Hello, World!"
 
-# Reversing the string
+    # Taking input from the user
+input_string = input("Enter a string to reverse: ")
+
+    # Reversing the string using the function
 reversed_string = reverse_string(input_string)
 
-# Displaying the reversed string
+    # Displaying the reversed string
 print("Reversed string:", reversed_string)
