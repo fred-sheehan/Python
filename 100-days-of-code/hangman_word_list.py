114cf8d15cd7585dc59955567c94d2faa30bee13
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-02 21:01:54 +0100

    tidying up folder structure

--- 100-days-of-code/hangman_word_list.py
+++
@@ -1,3 +0,0 @@
-# Hangman word list
-
-word_list = ["ardvark", "baboon", "camel", "dog", "elephant", "frog", "giraffe", "iguana", "jaguar", "kangaroo", "llama", "monkey", "newt", "octopus", "penguin", "lion", "rabbit", "snake", "tiger", "vulture", "walrus", "yak", "zebra"]
