7cda1f3051d51617c5248ab83e6b890f681e603c
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-02 13:11:37 +0100

    updated day_9_secret_auction.py and fixed error in gavel_logo.py

--- 100-days-of-code/day_9_secret_auction.py
+++ 100-days-of-code/day_9_secret_auction.py
@@ -1,19 +1,35 @@
 # Blind bid auction program
-import os
 from gavel_logo import logo
+import os
 
 def clear():
     os.system('cls||clear')
 
 clear()
+print("Welcome to the secret auction program.")
 print(logo)
 
-input("Welcome to the secret auction program.")
+bids = {}
+finished_bidding = False
 
-while True:
+def find_highest_bid(bidding_record):
+    # bidding_record = {"name": bid, "name": bid}
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
+while not finished_bidding:
     name = input("What is your name?: ")
-    bid = int(input("What's your bid?: $"))
-    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
-    if more_bidders == 'no':
-        break
-    clear()
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n ").lower()
+    if should_continue == "no":
+        finished_bidding = True
+        find_highest_bid(bids)
+    elif should_continue == "yes":
+        clear()
