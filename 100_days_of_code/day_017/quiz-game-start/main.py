530645dd47d04949b94b689ccbce5a34c99678a0
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-08 23:37:24 +0100

    quiz game updated after seeing angelas code

--- 100_days_of_code/day_017/quiz-game-start/main.py
+++ 100_days_of_code/day_017/quiz-game-start/main.py
@@ -18,3 +18,7 @@ quiz = QuizBrain(question_bank)
 # start quiz
 while quiz.still_has_questions():
     quiz.next_question()
+
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
