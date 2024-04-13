1f8260939658660cc05a4a2398ba6f4a593ec65c
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-13 07:45:47 +0100

    updated snake game to save highest recorded score

---
+++ 100_days_of_code/day_024/notes.py
@@ -0,0 +1,17 @@
+# Python read from a file
+
+with open("my_file.txt") as file:
+    content = file.read()
+    print(content)
+
+# Python write to a file - note the need of 'mode' parameter
+# iof file doesn't exist, it will be created!
+with open("new_file.txt", mode="w") as file:
+    file.write("\nAdd some text, play around, but do take care, is your judgment sound?")
+    print(file)
+
+# Python append to a file instead of overwriting it!
+
+with open("my_file.txt", mode="a") as file:
+    file.write("\n\nThat's much better, we kept our text, don't wipe it out, that's never best!")
+    print(file)
