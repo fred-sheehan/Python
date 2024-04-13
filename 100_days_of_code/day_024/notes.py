d6f8d5155cad92962b42a7de09a5653cd193deaa
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-13 21:22:03 +0100

    added project files day_24

--- 100_days_of_code/day_024/notes.py
+++ 100_days_of_code/day_024/notes.py
@@ -1,12 +1,12 @@
 # Python read from a file
 
-with open("my_file.txt") as file:
+with open("/Users/fred/Desktop/my_file.txt") as file:
     content = file.read()
     print(content)
 
 # Python write to a file - note the need of 'mode' parameter
 # iof file doesn't exist, it will be created!
-with open("new_file.txt", mode="w") as file:
+with open("/Users/fred/Desktop/new_file.txt", mode="w") as file:
     file.write("\nAdd some text, play around, but do take care, is your judgment sound?")
     print(file)
 
