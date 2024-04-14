804042ea0e949643f06129701fe5892b07e7a7f4
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-14 22:34:12 +0100

    tkinter files added day_027

--- 100_days_of_code/day_027/main.py
+++ 100_days_of_code/day_027/main.py
@@ -1,23 +1,35 @@
-# TKinter GUI program
-import tkinter as tk
-from tkinter import ttk
+# Miles to Km converter
+from tkinter import *
 
-window = tk.Tk()
-window.title("GUI is best")
-window.minsize(width=600, height=400)
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=400, height=150)
+window.config(padx=20, pady=20)
 
-# Create a label
-label = tk.Label(text="I am a label", font=("Arial", 24, "bold"))
-label.pack()
+enter_miles = Entry(width=10, font=("Arial", 24))
+enter_miles.grid(row=0, column=2)
 
-btn_standard = tk.Button(window, text='Standard Button')
-btn_standard.pack()
+miles_label = Label(text="Miles", font=("Arial", 24))
+miles_label.grid(row=0, column=3)
+miles_label.config(padx=20)
 
+is_equal = Label(text="is equal to", font=("Arial", 24))
+is_equal.grid(row=2, column=1)
+is_equal.config(padx=20)
 
+km_result = Label(text="0", font=("Arial", 24))
+km_result.grid(row=2, column=2)
+km_result.config(padx=20, pady=10)
 
-# Setting a theme for TTK widgets
-# ttk.Style().theme_use('alt')
-# btn_ttk = ttk.Button(window, text='TTK Button')
-# btn_ttk.pack()
+km_label = Label(text="Km", font=("Arial", 24))
+km_label.grid(row=2, column=3)
+
+def miles_to_km():
+    user_input = float(enter_miles.get())
+    km = round(user_input * 1.609, 1)
+    km_result.config(text=km)
+
+calculate_button = Button(text='Calculate', font=("Arial", 20), command=miles_to_km)
+calculate_button.grid(row=3, column=2)
 
 window.mainloop()
--- 100_days_of_code/day_027/notes.py
+++ 100_days_of_code/day_027/notes.py
@@ -31,3 +31,39 @@ class Car:
 my_car = Car(make='Toyota', year=2019, color='red')
 print(my_car.make) # prints Toyota
 print(my_car.model) # prints None rather than an error
+
+
+# TKinter GUI programming
+from tkinter import *
+
+window = Tk()
+window.title("GUI is best")
+window.minsize(width=600, height=400)
+window.config(padx=50, pady=50)
+# Create a label
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.grid(row=0, column=0)
+
+# change the text of the label directly or using the config method, last change will be the one displayed
+# my_label['text'] = 'New Text'
+my_label.config(text='New Label Text', padx=50, pady=50)
+
+def button_clicked():
+    user_input = entry.get()
+    my_label.config(text=user_input)
+
+# Create a button displayed in the window
+my_button = Button(text='Click Me', command=button_clicked)
+my_button.grid(row=2, column=2)
+
+my_2_button = Button(text='Click Me', command=button_clicked)
+my_2_button.grid(row=0, column=3)
+
+#Entries
+entry = Entry(width=30)
+#Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+#Gets text in entry, use entry.get() to get the text
+entry.grid(row=3, column=4)
+
+window.mainloop()
