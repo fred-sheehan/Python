2479fb512263bbbb7ce0a3b1bff05e3572fd44a8
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-14 18:14:32 +0100

    updates to project files

--- 100_days_of_code/day_026/notes.py
+++ 100_days_of_code/day_026/notes.py
@@ -1,11 +1,17 @@
-# list comprehension
+#For Loop, were going to replace this method with..
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+print(new_list) # [2, 3, 4]
+
+# .... list comprehension!
 # new_list = [new_item for item in list]
 # for example
-numbers = [1, 2, 3]
-print(numbers) # [1, 2, 3]
 new_numbers = [x + 1 for x in numbers]
 print(new_numbers) # [2, 3, 4]
-more_numbers = [item +10 for item in numbers]
+more_numbers = [item + 10 for item in numbers]
 print(more_numbers) # [11, 12, 13]
 
 # list comprehension with string
@@ -16,9 +22,9 @@ print(letters_list) # ['A', 'n', 'g', 'e', 'l', 'a']
 # list comprehension with range
 # new_list = [new_item for item in range]
 # for example
-squared_numbers = [x*x for x in range(1, 5)]
+squared_numbers = [x * x for x in range(1, 5)]
 print(squared_numbers) # [1, 4, 9, 16]
-doubled_numbers = [x*2 for x in range(1, 5)]
+doubled_numbers = [x * 2 for x in range(1, 5)]
 print(doubled_numbers) # [2, 4, 6, 8]
 
 # list comprehension with condition
@@ -26,7 +32,7 @@ print(doubled_numbers) # [2, 4, 6, 8]
 # for example
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 print(names) # ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-short_names = [name for name in names if len(name) < 5]
+short_names = [name for name in names if len(name) <= 5]
 print(short_names) # ['Alex', 'Beth', 'Dave']
 long_names = [name.upper() for name in names if len(name) > 5]
 print(long_names) # ['CAROLINE', 'ELEANOR', 'FREDDIE']
@@ -46,7 +52,7 @@ list_of_strings = nums_as_strings
 list_of_numbers = [int(x) for x in list_of_strings]
 print(list_of_numbers)
 
-# Use list comprehension to filter out the odd numbers (modulo)
+# Use list comprehension to filter out the odd numbers
 # and store the even numbers in a list called "result"
 result = [num for num in list_of_numbers if num%2 == 0]
 print(result)
@@ -62,3 +68,56 @@ with open("file2.txt") as file2:
 # convert the strings to integers and check the common numbers
 result = [int(num) for num in list1 if num in list2]
 print(result)
+
+# dictionary comprehension
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+print(names)
+
+import random
+# new_dictionary = {new_key:new_value for item in list}
+students_scores = {student:random.randint(1, 100) for student in names}
+print(students_scores)
+
+# new_dictionary = {new_key:new_value for (key, value) in dictionary.items()}
+
+# new_dict = {new_key:new_value for (key, value) in dictionary.items() if test}
+passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
+print(passed_students)
+
+# create dictionary from input, split to words with character count
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# new_dictionary = {new_key:new_value for new_key in dictionary}
+result = {word:len(word) for word in sentence.split()}
+print(result)
+
+# create new dictionary from weather_c dictionary converted to temperature in F
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+print(weather_c)
+# To convert temp_c into temp_f use this formula:
+#(temp_c * 9/5) + 32 = temp_f
+
+# new_dictionary = {new_key:new_value for (key, value) in dictionary.items()}
+weather_f = {day:temp_c * 9/5 + 32 for (day, temp_c) in weather_c.items()}
+print(weather_f)
+
+# looping through dictionaries - create a dictionary
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+# looping through the dictionary
+for (key, value) in student_dict.items():
+    print(value)
+
+# now using pandas, first create a pandas data frame
+import pandas as pd
+student_data_frame = pd.DataFrame(student_dict)
+print(student_data_frame)
+
+# then loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # print(row)
+    # print(row.student)
+    # print(row.score)
+    if row.student == "Angela":
+        print(row.score)
