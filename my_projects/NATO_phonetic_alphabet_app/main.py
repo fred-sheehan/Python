78425fc9c4d96952539b2753a0960f0668075ca3
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-16 16:32:04 +0100

    updated code to include error handling

--- my_projects/NATO_phonetic_alphabet_app/main.py
+++ my_projects/NATO_phonetic_alphabet_app/main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
-print(data)
 
 nato_phonetic_alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
 
-user_word = input("Enter a word: ").upper()
+print("Type in a word and have the NATO phonetic alphabet equivalent displayed.")
 
-output_word_list = [nato_phonetic_alphabet[letter] for letter in user_word]
+while True:
+    user_word = input("Enter a word: ").upper()
+    try:
+        output_word_list = [nato_phonetic_alphabet[letter] for letter in user_word]
+        break
+    except KeyError:
+        print("Letters only please.")
 
 print(output_word_list)
