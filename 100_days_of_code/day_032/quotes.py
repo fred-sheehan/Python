3ee7a84f079e20341a0a152797f5a247b69766bd
Fred Sheehan <fredsheehan@live.co.uk>
2024-04-24 11:49:11 +0100

    day_032 files added

---
+++ 100_days_of_code/day_032/quotes.py
@@ -0,0 +1,30 @@
+import smtplib
+import datetime as dt
+import random
+
+# gmail smtp server
+MY_EMAIL = 'random_email@gmail.com'
+MY_PASSWORD = 'insert email password here'
+
+# whatever day today is, send an auto email motivational qoute to yourself via email from the quotes.txt file
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 2:
+    with open("quotes.txt") as quote_file:
+            all_quotes = quote_file.readlines()
+            todays_quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(
+            user=MY_EMAIL,
+            password=MY_PASSWORD
+        )
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            # send to live mail
+            to_addrs='recipient@email.com',
+            msg=f"Subject:Today's Quote\n\n{todays_quote}"
+        )
